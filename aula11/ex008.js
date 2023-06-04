173bdf8d0350c7df7208e1ce135c7f2c1be6c83a
otavio <barbosaotavio293@gmail.com>
2023-06-04 01:18:56 -0300

    mensagem de teste

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`voce ultrapassou a velocidade premitida. MULTADO!`)
+}
+console.log(`dirija sempre com o cinto de segurança`)
